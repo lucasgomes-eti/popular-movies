39ea2c2de01ddb1a8f6228e91ee37087297f670a
Lucas Gomes <lucas.gomes@nuvem.net>
2020-04-09 15:13:06 -0400

    Add launcher icon

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ export const environment = {
         images_base_url: 'https://image.tmdb.org/t/p/w500',
         youtube_thumb_base_url: 'https://img.youtube.com/vi',
         youtube_video_base_url: 'https://www.youtube.com/watch?v=',
-        api_key: ''
+        api_key: '1db4f4cccc63b2f2bdb0cdf1aa97dd4f'
     },
     routes: {
         popular_movies: '/movie/popular',
